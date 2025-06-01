2a43fde98c4a8251ba24de6dabe5036a8c505116
Juan Manuel Castro Arnez <manuelca@yahoo.com>
2025-06-01 15:13:40 -0400

    Real result for GFC vs ULT added.

--- js/data/MatchResults.js
+++ js/data/MatchResults.js
@@ -8,6 +8,7 @@ export class MatchResults {
                 "H": { winner: "ZTA", loser: "OUN", winnerGoals: 5, loserGoals: 2 },
                 "N": { winner: "PAS", loser: "ULC", winnerGoals: 5, loserGoals: 4 },
                 "F": { winner: "GDC", loser: "ERA", winnerGoals: 5, loserGoals: 4 },
+                "C": { winner: "ULT", loser: "GFC", winnerGoals: 3, loserGoals: 1 },
                 // "A": { winner: "ZAY", loser: "DMG", winnerGoals: 3, loserGoals: 1 },
                 // "B": { winner: "PRS", loser: "LCA", winnerGoals: 2, loserGoals: 0 }
             },
@@ -22,7 +23,7 @@ export class MatchResults {
             final: {}
         };
         this.realResults = new Set([
-            'round1.P', 'round1.A', 'round1.H', 'round1.N', 'round1.F', //'round2Winners.1'
+            'round1.P', 'round1.A', 'round1.H', 'round1.N', 'round1.F', 'round1.C', //'round2Winners.1'
             // Add more real result identifiers as needed
         ]);
     }
